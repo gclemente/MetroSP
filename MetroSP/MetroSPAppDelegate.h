cb1b84b6b3468d8a0d58e06ba24d37e9b8d81a4e
Gustavo Clemente <gclemente@livetouch.com.br>
2011-09-27 07:42:46 -0300

    Initial Commit

---
+++ MetroSP/MetroSPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MetroSPAppDelegate.h
+//  MetroSP
+//
+//  Created by Gustavo Clemente on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MetroSPAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ MetroSP/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MetroSP
+//
+//  Created by Gustavo Clemente on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
